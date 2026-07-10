70363050da2c3d4220481323e5ee55e9941bda39
felrfn <ardiansyahrafael5@gmail.com>
2026-07-11 01:29:55 +0700

    md2,3, sosmas

--- app/kkn/page.tsx
+++ app/kkn/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/app/components/Navbar";
 import TeamSection from "@/app/components/TeamSection";
+import KknProgramsSection from "@/app/components/KknProgramsSection";
 import AboutSection from "@/app/components/AboutSection";
 import UmkmShowcase from "@/app/components/UmkmShowcase";
 import Footer from "@/app/components/Footer";
@@ -17,6 +18,10 @@ export default function KknPage() {
       <div className="flex-1 flex flex-col pt-16">
         {/* Render team first as requested */}
         <TeamSection />
+        
+        {/* Render Multidisiplin 2, 3 & 15 Program Sosmas (Tandang & Jangli) */}
+        <KknProgramsSection />
+
         <AboutSection />
         <UmkmShowcase />
       </div>
@@ -25,3 +30,4 @@ export default function KknPage() {
     </>
   );
 }
+
